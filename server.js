add452d73cc72a5e49bf82a2424d0b82a13b16de
Key Hui <key.hui@neoderm.com.hk>
2017-10-25 09:45:13 +0800

    no message

--- server.js
+++ server.js
@@ -12,5 +12,16 @@ app.get('/', (req, res) => {
   res.send('Hello world abc\n');
 });
 
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'hehe_is_fun') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
