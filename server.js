c2d1ec2b7b345c216ea93e654a51745b539e26a3
Key Hui <key.hui@neoderm.com.hk>
2017-10-25 18:30:34 +0800

    no message

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 // bot framework
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3000, function () {
+server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
