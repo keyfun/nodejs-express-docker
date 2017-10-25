6cc4472824c667bf6e65737753c06a571f04c535
Key Hui <key.hui@neoderm.com.hk>
2017-10-25 18:26:19 +0800

    no message

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var forceSsl = require('express-force-ssl');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// bot framework
+var restify = require('restify');
+var builder = require('botbuilder');
+
 // Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
@@ -19,6 +23,25 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('Hello world abc\n');
 });
+
+
+// bot framework
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
+
+
+
 
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
