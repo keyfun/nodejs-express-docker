ef2d61e7dd8bca67d110592ff41f41b5538cd75b
Key Hui <key.hui@neoderm.com.hk>
2017-10-27 09:42:14 +0800

    Auto Push

--- server.js
+++ server.js
@@ -33,11 +33,10 @@ server.listen(process.env.port || process.env.PORT || PORT, function () {
 });
 
 // Create chat connector for communicating with the Bot Framework Service
-// var connector = new builder.ChatConnector({
-//     appId: process.env.MICROSOFT_APP_ID,
-//     appPassword: process.env.MICROSOFT_APP_PASSWORD
-// });
-var connector = new builder.ConsoleConnector().listen();
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
 
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
